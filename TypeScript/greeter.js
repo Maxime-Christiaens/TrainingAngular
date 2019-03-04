53c23fabb0943c057a50cd657dbcab5231c3b0f9
Maxime Christiaens <maxime.ad.christiaens@gmail.com>
2019-03-04 11:03:56 +0100

    typage et class en typescript

--- TypeScript/greeter.js
+++ TypeScript/greeter.js
@@ -1,5 +1,15 @@
+var Student = /** @class */ (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+    return Student;
+}());
 function greeter(person) {
-    return "Hello, " + person;
+    return "Hello, " + person.firstName + " " + person.middleInitial + " " + person.lastName;
 }
-var user = "Max";
+//réutilise la class Student
+var user = new Student("Max", "The", "User");
 document.body.innerHTML = greeter(user);
--- TypeScript/greeter.ts
+++ TypeScript/greeter.ts
@@ -1,5 +1,20 @@
-function greeter(person :string){
-    return "Hello, " + person;
+class Student {
+    fullName: string;
+    constructor(public firstName: string, public middleInitial: string, public lastName: string){
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
 }
-let user = "Max";
+
+interface Person{
+    firstName: string; 
+    middleInitial: string; 
+    lastName: string;
+}
+
+function greeter(person: Person){
+    return "Hello, " + person.firstName + " " + person.middleInitial + " " + person.lastName;
+}
+//réutilise la class Student
+let user = new Student ("Max", "The", "User");
+
 document.body.innerHTML = greeter(user);
